89c762f568c9e328960d5380f1cd98d0e12cb3d1
David-Stelter <stelterd05@gmail.com>
2025-01-26 22:53:11 +0100

    Fibonacci-Elemente

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+fibo1 = 0
+fibo2 = 1
+
+while True:
+    try:
+        limit = int(input("Bitte geben Sie die Anzahl der Fibonacci-Zahlen ein, die Sie haben möchten: "))
+        break
+    except ValueError:
+        print("Bitte gib eine ganze Zahl ein.")
+
+for i in range(limit):
+    if i == limit - 1:
+        print(fibo1)
+    else:
+        print(fibo1, end=", ")
+    fibo1, fibo2 = fibo2, fibo1 + fibo2
+
