db9fc6d81b0d4de143a774f42d555dacc95bc558
David-Stelter <stelterd05@gmail.com>
2025-04-07 20:56:55 +0200

    Verschiedene Ausgaben mit Validierung

--- wort_zähler.py
+++ wort_zähler.py
@@ -1,6 +1,14 @@
 from collections import Counter
 worte = []
 
+def ausführliche_ausgabe(wort_anzahl):
+    for wort in wort_anzahl:
+        print(f"'{wort}' kommt {wort_anzahl[wort]}-mal im Text vor.")
+
+def kompakte_ausgabe(wort_anzahl):
+    for wort in wort_anzahl:
+        print(f"{wort}: {wort_anzahl[wort]}")
+
 text = input("Geben Sie einen Text ein oder q wenn Sie einen Dateinamen eingeben wollen: ")
 if text.lower() == "q":
     dateiname = input("Geben Sie den Dateinamen ein: ")
@@ -11,4 +19,19 @@ text = text.lower().replace(".", "").replace(",", "").replace("!", "").replace("
 worte = text.split()
 
 wort_anzahl = Counter(worte)
-print(wort_anzahl)
+while True:
+    try:
+        kompakt = input("Wollen sie eine kompakte Ausgabe? [j/N]: ").lower()
+        if kompakt == "j":
+            kompakte_ausgabe(wort_anzahl)
+            break
+
+        elif kompakt == "n":
+            ausführliche_ausgabe(wort_anzahl)
+            break
+
+        else:
+            raise ValueError
+
+    except ValueError:
+        print("Bitte gib j oder n ein.")
